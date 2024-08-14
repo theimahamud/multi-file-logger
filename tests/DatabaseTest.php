a19366d395439e8a64dfb2ac66ad55f9810f787b
rubelmahamud <rubelmahamud9997@gmail.com>
2024-08-14 15:42:46 +0600

    update from local

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rubel9997\MultiFileLogger\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase {}
---
+++ tests/StreamFileTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rubel9997\MultiFileLogger\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class StreamFileTest extends TestCase {}
